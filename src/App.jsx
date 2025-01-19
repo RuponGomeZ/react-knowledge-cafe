e5c057c33fed7723c87be77fec42df5fc1bbd64d
RuponGomeZ <anthonyrupon0@gmail.com>
2025-01-20 03:23:14 +0600

    blog added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
@@ -5,13 +6,19 @@ import Header from './Components/Header/Header'
 
 function App() {
 
+  const [bookmarks, setBlookmarks] = useState([]);
+
+  const handleAddToBookmark = blog => {
+    console.log("adding soon");
+
+  }
 
   return (
     <>
 
       <Header></Header>
-      <div className=' md:flex'>
-        <Blogs></Blogs>
+      <div className=' md:flex max-w-7xl mx-auto'>
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
         <Bookmarks></Bookmarks>
       </div>
     </>
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, reading_time, author_img, author, posted_date, hashtags } = blog;
     return (
-        <div>
-            <h1>new</h1>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt='${title}' />
+            <div className='flex justify-between mb-4'>
+                <div className='flex gap-6'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} Min Read</span>
+                    <button className='ml-2 text-2xl'><FaRegBookmark></FaRegBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
